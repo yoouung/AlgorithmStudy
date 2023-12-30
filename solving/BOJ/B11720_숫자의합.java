7f7d1250d004152eaae7fe603854462472dd96b0
yoouung <jyp101400@gmail.com>
2023-12-30 18:30:36 +0900

    231230 배열과 리스트

---
+++ solving/BOJ/B11720_숫자의합.java
@@ -0,0 +1,20 @@
+package solving.BOJ;
+import java.util.Scanner;
+
+public class B11720_숫자의합 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        String sNum = scanner.next();
+        scanner.close();
+
+        char[] num_list = sNum.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < x; i++) {
+            sum += num_list[i] - '0'; // char to int
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ solving/BOJ/B1546_평균.java
@@ -0,0 +1,22 @@
+package solving.BOJ;
+import java.util.*;
+
+public class B1546_평균 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        long sum = 0;
+        long max = 0;
+        
+        for (int i = 0; i < x; i++) {
+            int score = scanner.nextInt();
+            if (score > max) {
+                max = score;
+            }
+            sum += score;
+        }
+
+        System.out.println(sum * 100.0 / max  / x);
+        
+    }
+}
