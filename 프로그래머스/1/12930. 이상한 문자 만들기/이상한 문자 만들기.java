d46cd32b85431d993586883f8c52db3a59203169
Park Jaeyoung <78146904+yoouung@users.noreply.github.com>
2024-10-20 19:00:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.13 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    // char은 기본자료형이기 때문에,
+    // array[i].toUpperCase() 처럼 메소드 호출이 안됨
+    // -> Character.toUpperCase(array[i])와 같이 Character 클래스의 정적 메소드를 사용해야 한다.
+    
+    // " " 은 String, ' '은 char
+    
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        int tempIndex = 0;
+        for (int i=0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            
+            if (current == ' ') {
+                tempIndex = 0;
+                sb.append(current);
+                continue;
+            }
+            
+            if (tempIndex % 2 == 0) {
+                sb.append(Character.toUpperCase(current)); 
+            } else {
+                sb.append(Character.toLowerCase(current));
+            }
+            tempIndex++;
+        }
+        
+        return sb.toString();
+    }
+}
