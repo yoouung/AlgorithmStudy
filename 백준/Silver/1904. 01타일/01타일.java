f86b7cf11366e729a0ce7a985fbd62824453c9e3
Park Jaeyoung <78146904+yoouung@users.noreply.github.com>
2025-06-02 09:11:54 +0900

    [Silver III] Title: 01타일, Time: 128 ms, Memory: 18200 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  /*
+  DP
+  1. dp[i-1] + '1' : 1가지
+  2. dp[i-2] + '00': 1가지
+  */
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] dp = new int[N+1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i=2; i<=N; i++) {
+      dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+    }
+
+    System.out.println(dp[N]);
+  }
+}
