489a729afcf7071885664779868860d5b3a66216
Park Jaeyoung <78146904+yoouung@users.noreply.github.com>
2025-06-05 20:19:50 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,8 +3,11 @@ import java.io.*;
 
 public class Main {
   /*
-  dp....? 냅색...?
+  dp....?
   이전 상담 다 돌아보면서 현재 날짜까지 가능한 상담비 계산
+
+  시간복잡도 줄이기:
+  - 거꾸로 DP (i + time[i]가 N+1 이하여야 한다. 즉, 뒤에서부터 보면 앞으로 갈 수 있는 경우가 명확해짐)
   */
 
   public static void main(String[] args) throws IOException {
@@ -18,6 +21,17 @@ public class Main {
       pay[i] = Integer.parseInt(st.nextToken());
     }
 
+    int[] dp = new int[N + 2]; // i일부터 시작했을 때의 최대 수익
+    for (int i = N; i >= 1; i--) {
+      int nextDay = i + time[i];
+      if (nextDay <= N + 1) {
+        dp[i] = Math.max(pay[i] + dp[nextDay], dp[i + 1]); // 상담 O vs 상담 X
+      } else {
+        dp[i] = dp[i + 1]; // 상담 불가 → 이전 값 유지
+      }
+    }
+
+    /* 시간복잠도 O(n^2)
     int[] max = new int[N + 1];
     for (int i = 1; i <= N; i++) {
       max[i] = max[i - 1];
@@ -27,7 +41,8 @@ public class Main {
         }
       }
     }
+     */
 
-    System.out.println(max[N]);
+    System.out.println(dp[1]);
   }
 }
